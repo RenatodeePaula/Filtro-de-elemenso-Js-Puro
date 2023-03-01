dafd51da3c0cc5c52fff24aafa3f804c0d7a389c
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-03-01 12:59:13 -0300

    add README.md

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// get filter element
+
 let filter = document.querySelector('input')
-// get cards elements
 let cards = document.querySelectorAll('.cards .card')
 filter.addEventListener('input', filterFunction)
 
@@ -11,7 +10,7 @@ function filterFunction () {
     for (let card of cards) {     
       let title = card.querySelector('h2')        
       title = title.textContent.toLowerCase()  
-          
+
       let filterText = filter.value.toLowerCase()
       
       if (!title.includes(filterText)) {        
@@ -19,21 +18,12 @@ function filterFunction () {
       } else{
         card.style.display = 'block'
       }    
-    }    
-
+    }
+    
   } else {
     for (let card of cards) {
       card.style.display = 'block'
-
     }
-
-   
-
-  }
-  
+  }  
 }
 
-      
-  // else
-    // for each card of cards
-      // unhide the card element    cards.style.background = 'blue'
