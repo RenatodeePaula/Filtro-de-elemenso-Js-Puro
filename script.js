a5defe2856c90c8b8d9240dfbd22be8a32306bbf
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-02-28 21:43:20 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
 // get filter element
-let input = document.querySelector('input')
-
+let filter = document.querySelector('input')
 // get cards elements
-let cards = document.querySelectorAll('.cards .card')
-
+let cards = document.querySelectorAll('.cards', '.card')
 // add input event for the filter element
-input.addEventListener('input', filterFunction)
-
+filter.addEventListener('input', filterFunction)
 // filter function
 function filterFunction () {
-    // if the filter is not empty
-    if (!input.value == "") {
-        // for each card of cards
-        for (let card of cards) {
-            // get card heading (title)
-            let title = document.querySelector('.card h2')
-            // tranform to lower case
-            title = title.textContent.toLowerCase()
-            // transform filter text to lower case
-            let filterInput = input.value.toLowerCase()
-            // if card title does not include the filter text
-            console.log(title)
+  // if the filter is not empty
+  if (filter.value != '') {
+    // for each card of cards
+    for (let card of cards) {
+      // get card heading (title)
+      let title = document.querySelector('h2')
+      // tranform to lower case
+      title.textContent.toLowerCase()
+      // transform filter text to lower case
+     
 
-            // if (!title.includes(filterInput)) {
-            //     // hide the card element
-            //     card.style.display = 'none'
+      }
+    
 
+  } else {
+   
 
-            // } else {
-            //     card.style.display = 'block'
-
-            // }
-             
-
-        }
-       
-
-    }
-
-
+  }
+  
 }
+
+      // if card title does not include the filter text
+        // hide the card element
       // else
         // unhide the card element
   // else
     // for each card of cards
-      // unhide the card element
+      // unhide the card element    cards.style.background = 'blue'
