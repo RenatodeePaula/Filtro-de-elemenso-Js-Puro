b4b4aead90fde01d9e550fbfa887ad118145dd80
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-03-01 11:20:04 -0300

    commit final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // get filter element
 let filter = document.querySelector('input')
 // get cards elements
-let cards = document.querySelectorAll('.cards', '.card')
+let cards = document.querySelectorAll('.cards .card')
 // add input event for the filter element
 filter.addEventListener('input', filterFunction)
 // filter function
@@ -11,26 +11,36 @@ function filterFunction () {
     // for each card of cards
     for (let card of cards) {
       // get card heading (title)
-      let title = document.querySelector('h2')
+      let title = card.querySelector('h2')
       // tranform to lower case
-      title.textContent.toLowerCase()
+      title = title.textContent.toLowerCase()
       // transform filter text to lower case
-     
+      let filterText = filter.value.toLowerCase()
+      // if card title does not include the filter text
+      if (!title.includes(filterText)) {
+        // hide the card element
+        card.style.display = 'none'
+
+      } else{
+        card.style.display = 'block'
 
       }
-    
+      // unhide the card element
+    }    
 
   } else {
+    for (let card of cards) {
+      card.style.display = 'block'
+
+    }
+
    
 
   }
   
 }
 
-      // if card title does not include the filter text
-        // hide the card element
-      // else
-        // unhide the card element
+      
   // else
     // for each card of cards
       // unhide the card element    cards.style.background = 'blue'
